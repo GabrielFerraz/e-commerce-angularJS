08ec52a40edf533d21ede466d00d853a8e5aa6da
Gabriel <gabrielmaiaferraz@gmail.com>
2017-11-25 13:27:03 +1300

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { RoundProfileImageComponent } from './round-profile-image/round-profile-image.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    RoundProfileImageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    HeaderComponent,
+    HomeComponent
+  ]
 })
 export class AppModule { }
